55b44416e50ad5b566a7fba9d8e8349c57a18411
Nicolas Marini <nmarini44@gmail.com>
2018-05-28 22:19:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,7 @@ def sum_array(array)
   array.inject{|result, element| result + element}
 end
 
-def adds(array)
+def add_s(array)
   array.each_with_index.collect do |element, index|
 if element[index] == element[1]
 element
