ca572ba64116f0fd0940db9581c625351b600d52
Nicolas Marini <nmarini44@gmail.com>
2018-05-28 20:31:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort{|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+array.sort{|a, b| a.size <=> b.size }
+end
+
+def swap_elements(array)
+array.map do
+end
+
+def reverse_array(array)
+  array.reverse
+end
